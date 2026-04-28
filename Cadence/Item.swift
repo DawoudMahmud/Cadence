a95c31b4a924afd11b3b93808417b090c0cb7f47
Dawoud Mahmud <dmahmud6@gatech.edu>
2026-04-27 20:17:28 -0700

    Initial Commit

---
+++ Cadence/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Cadence
+//
+//  Created by Dawoud Mahmud on 4/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CadenceTests/CadenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  CadenceTests.swift
+//  CadenceTests
+//
+//  Created by Dawoud Mahmud on 4/27/26.
+//
+
+import Testing
+@testable import Cadence
+
+struct CadenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
